cfcc311fddbe4bace0de4e22fff3d03deec23223
Zack Macharia <blaqhrse@gmail.com>
2020-07-25 13:11:00 -0500

    initial folder commit

---
+++ build_venv/setup_venv.py
@@ -0,0 +1,10 @@
+"""
+    This function will build a virtual environment using the standard python3 virtualevironmnet module.
+
+    Prompt user for project/folder name - this will be the base directory for the virtualenvironment
+    Verify name doesn't exist in the current location
+    Create a new directory using the user's input value
+    CD to the new directory
+    Initiate git
+    Create a new virtualenvironment
+"""
