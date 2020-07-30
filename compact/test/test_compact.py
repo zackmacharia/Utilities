6555c4f2070e5d804d55445f784a6b9647f2a086
Zack Macharia <blaqhrse@gmail.com>
2020-07-29 22:24:01 -0500

    modified test function

--- compact/test/test_compact.py
+++ compact/test/test_compact.py
@@ -3,5 +3,5 @@ from compact.code.compact import del_adjacent_dups
 def test_del_adjacent_dups():
 
     test_list = [1, 1, 2, 2, 3, 2]
-    expected_output = [1, 2, 3, 2]
-    assert del_adjacent_dups(test_list) == expected_output
+    c = del_adjacent_dups(test_list)
+    assert iter(c) == c
