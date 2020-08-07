84f0723ce39a2233b0eac71cd7d6f317710dd155
Zack Macharia <blaqhrse@gmail.com>
2020-08-06 20:34:24 -0500

    updated README and added print statement to compact.py

--- compact/code/compact.py
+++ compact/code/compact.py
@@ -11,8 +11,6 @@ def del_adjacent_dups(sequence):
 
 if __name__ == '__main__':
     sequence = [1, 1, 2, 2, 3, 2]
-    del_adjacent_dups(sequence)
-    # c = del_adjacent_dups(n**2 for n in [1, 2, 2])
-    # print(iter(c) is c)
-    # print(next(c))
-
+    generator_object = del_adjacent_dups(sequence)
+    for i in generator_object:
+        print(i)
